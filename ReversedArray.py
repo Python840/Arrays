fe2a676549879fa0d22c3f3be827adeb637448c1
iHunter <51865090+Python840@users.noreply.github.com>
2023-10-10 23:21:43 +0200

    Update ReversedArray.py

--- ReversedArray.py
+++ ReversedArray.py
@@ -1,13 +1,18 @@
+# Function to reverse an array in-place
 def reverse(A, start, end):
     while start < end:
+        # Swap the elements at the 'start' and 'end' positions
         A[start], A[end] = A[end], A[start]
+        # Increment 'start' (moving from 0 towards the center)
         start += 1
+        # Decrement 'end' (moving from the end towards the center)
         end -= 1
 
-
+# Example usage
 A = [1, 2, 3, 4, 5, 6]
-end_arr = len(A) - 1
-print(f"given array is:\n{A}")
+end_arr = len(A) - 1  # Get the index of the last element
+print(f"Given array is:\n{A}")
 
-reverse(A, 0, end_arr)
+# Call the 'reverse' function to reverse the array
+reverse(A, 0, end_arr)  # Using variables for clarity
 print(f"Reversed array:\n{A}")
